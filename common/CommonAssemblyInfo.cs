8e38786c74df95a3e430dd91ddfc7cf164404430
Vitaly Mikhailov <personal@aragas.org>
2020-02-29 01:29:35 +0300

    Dropped actually PCL support due to Span usage
    Updated nuget info

--- common/CommonAssemblyInfo.cs
+++ common/CommonAssemblyInfo.cs
@@ -3,9 +3,9 @@ using System.Reflection;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Aragas.Network-2")]
-[assembly: AssemblyCopyright("Copyright ©  2017-2018")]
+[assembly: AssemblyCopyright("Copyright ©  2017-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.2.1.0")]
-[assembly: AssemblyFileVersion("2.2.1.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
